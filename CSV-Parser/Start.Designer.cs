ffd5c2d7b0055e268c1a375c14dabc45377a2638
Justin <justin@garofolo.net>
2018-12-26 23:28:04 -0500

    column selection (not done)

--- CSV-Parser/Start.Designer.cs
+++ CSV-Parser/Start.Designer.cs
@@ -24,29 +24,81 @@
         /// </summary>
         private void InitializeComponent() {
             this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlColumnList = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ofd
             // 
             this.ofd.Filter = "CSV files (*.csv)|*.csv";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "file: {0}";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "please select the columns you\'d like parse.";
+            // 
+            // pnlColumnList
+            // 
+            this.pnlColumnList.BackColor = System.Drawing.Color.Black;
+            this.pnlColumnList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlColumnList.Location = new System.Drawing.Point(15, 44);
+            this.pnlColumnList.Name = "pnlColumnList";
+            this.pnlColumnList.Size = new System.Drawing.Size(353, 157);
+            this.pnlColumnList.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(15, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(353, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "continue";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
-            this.ClientSize = new System.Drawing.Size(438, 82);
+            this.ClientSize = new System.Drawing.Size(380, 248);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pnlColumnList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
             this.Name = "Start";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CSV Parser";
             this.Load += new System.EventHandler(this.Start_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pnlColumnList;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CSV-Parser/Start.cs
+++ CSV-Parser/Start.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,16 +15,74 @@ namespace CSV_Parser {
     public partial class Start : Form {
 
         private string FilePath;
+        private List<CheckBox> ColumnChecks;
 
         public Start() {
 
             InitializeComponent();
 
+            // don't allow horizontal scroll bar (not needed)
+            pnlColumnList.HorizontalScroll.Enabled = false;
+            pnlColumnList.HorizontalScroll.Visible = false;
+            pnlColumnList.HorizontalScroll.Maximum = 0;
+
+            // enable auto-scroll (allow for vertical scroll bar, if needed)
+            pnlColumnList.AutoScroll = true;
+
+        }
+
+        private void Failed(string reason) {
+            MessageBox.Show(reason, "CSV Parser", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Close();
         }
 
         private void Start_Load(object sender, EventArgs e) {
 
-            SelectCSV();
+            // make sure they've selected a csv file
+            bool selected = SelectCSV();
+            if (!selected) {
+                Failed("Issue occurred while selecting .csv file.");
+                return;
+            }
+
+            // parse information from csv
+            using (TextFieldParser parser = new TextFieldParser(FilePath)) {
+
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                if (parser.EndOfData) {
+                    Failed("Unable to resolve .csv data columns.");
+                    return;
+                }
+
+                // read the first row of data from the .csv file
+                // this generally contains the column names
+                string[] fields = parser.ReadFields();
+
+                const int Y_PADDING = 5;
+                const int X_OFFSET = 7;
+                int Y_OFFSET = 7;
+
+                ColumnChecks = new List<CheckBox>();
+
+                for (int i = 0; i < fields.Length; i++) {
+
+                    string field = fields[i];
+
+                    // initialize checkbox component
+                    CheckBox cb = new CheckBox();
+                    cb.Location = new Point(X_OFFSET, Y_OFFSET);
+                    cb.AutoSize = true;
+                    cb.Text = field;
+
+                    // add to panel and account for y offset/padding
+                    ColumnChecks.Add(cb);
+                    pnlColumnList.Controls.Add(cb);
+                    Y_OFFSET += cb.Size.Height + Y_PADDING;
+
+                }
+
+            }
 
         }
 
